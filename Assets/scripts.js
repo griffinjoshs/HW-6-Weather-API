24a9a9fbf60f3a7297189313c42df69eb4dc9f85
Griffin Surett <leveloutusa@gmail.com>
2020-10-26 01:47:00 -0400

    Forloop done and weather displayed

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -26,8 +26,9 @@ $(document).ready(function () {
             var wind = $("#wind");
             // Convert the temp to fahrenheit
             var tempF = ((response.main.temp - 273.15) * 1.80 + 32).toFixed(2);
+            var currentDate = moment().format('l');
 
-            location.append(city)
+            location.append(city + " " + currentDate)
             temp.append('temperature: ' + tempF)
             humidity.append('humidity: ' + response.main.humidity)
             wind.append('wind: ' + response.wind.speed)
@@ -50,30 +51,79 @@ $(document).ready(function () {
                     var uv = $("#uv");
                     uv.append('uv index: ' + response.value)
                 })
-            
+
             // 5 day forecast api declaration
             var forecast5queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=b6d12dfce99afcda449f11a91371bcfd"
-                    console.log(forecast5queryURL);
-              // 5 day forecast ajax
-              $.ajax({
+            console.log(forecast5queryURL);
+            // 5 day forecast ajax
+            $.ajax({
                 url: forecast5queryURL,
                 method: "GET"
 
             }).then(function (response) {
                 console.log(response);
+
+                // counter for forloop
+                var startMyForecast = 1;
+
+                //object array
+                var fiveDayForecast = [
+                    {
+                        day: $("#day1"),
+                        temp: $("#day1temp"),
+                        humidity: $("#day1humidity"),
+                    },
+                    {
+                        day: $("#day2"),
+                        temp: $("#day2temp"),
+                        humidity: $("#day2humidity"),
+                    },
+                    {
+                        day: $("#day3"),
+                        temp: $("#day3temp"),
+                        humidity: $("#day3humidity"),
+                    },
+                    {
+                        day: $("#day4"),
+                        temp: $("#day4temp"),
+                        humidity: $("#day4humidity"),
+                    },
+                    {
+                        day: $("#day5"),
+                        temp: $("#day5temp"),
+                        humidity: $("#day5humidity"),
+                    },
+                ]
+
+                for (var i = 0; i < fiveDayForecast.length; i++) {
+                    var currDate = moment().add(startMyForecast, "day");
+                    currDate = String(currDate);
+                    currDate = currDate.substr(0, 15);
+                    fiveDayForecast[i].day.append(currDate);
+                    startMyForecast++
+                }
+
+                var objCounter = 0;
+
+                //5 day forcast for loop
+                for (var i = 1; i < 6; i++) {
+                    // Convert the temp to fahrenheit
+                    var tempF = ((response.daily[i].temp.day - 273.15) * 1.80 + 32).toFixed(2);
+                    fiveDayForecast[objCounter].temp.append("temperature:" + tempF + "°F")
+                    fiveDayForecast[objCounter].humidity.append("humidity:" + response.daily[i].humidity + "%")
+
+                }
+
+                // var date = new Date(response.daily[1].dt * 1000).toDateString();
+                // console.log('thedate', date)
+
                 // All Code That will add data to the html will go here
-                var day1 = $("#day1");
-                day1.append('Day 1: ' + response.daily[0].temp.day)
+
+
             })
 
-            // 5 day forcast for loop
-            for (var i = 0; i < response.list; i++){
-                var forecast = response.list[i]
-                forecast.temp.day
-                forecast.humidity
-                forecast.wind_speed
-                forecast.uvi
-            }
+
+            //day1.append('Temp: ' + response.daily.temp.day)
 
         })
     })
